3e19c0218773e88091b21f3452379eb1d1a70ef2
jorge <jorgemalaquia@gmail.com>
2023-02-02 17:53:52 -0300

    feat: basic CRUD routes added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
 
 
 const app = fastify();
+const prisma = new PrismaClient();
 
+app.register(cors);
 
-
+app.get('/habits', async ()=>{
+    return await prisma.habit.findMany();
+});
+app.post('/habits', async ()=>{
+    await prisma.habit.create({
+        data:{
+            name: 'Beber 3l água'
+        }
+    })
+});
 
 app.listen({
     port: 3333
